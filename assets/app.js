121d8b3b4138240357eb0ed561ce10facbe844d2
Mathis <mathisleskateur@gmail.com>
2024-01-30 21:35:10 +0100

    First Commit

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +5,10 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import $ from 'jquery';
+import 'jquery-ui/dist/jquery-ui.js';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
